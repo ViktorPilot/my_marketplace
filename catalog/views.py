06313302cb5b5a22353e53159cbb820591a66011
Viktor Bukotin <viktorbukotin0@gmail.com>
2026-03-30 17:39:58 +0300

    before lint

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse, HttpRequest
+
 from django.shortcuts import render
 
-# Create your views here.
+def home(request: HttpRequest) -> HttpResponse:
+    """Функция, рендерирующая страницу 'home'"""
+    return render(request, 'catalog/home.html')
+
+def contacts(request: HttpRequest) -> HttpResponse:
+    """Функция, рендерирующая страницу 'contacts'"""
+    return render(request, 'catalog/contacts.html')
+
+def contacts_post(request: HttpRequest) -> HttpResponse:
+    """Функция, рендерирующая ответ пользователю на его POST сообщение"""
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        return HttpResponse(f'Данные пользователя {name} ({phone, message}) успешно приняты)!')
+    return render(request, 'catalog/contacts.html')
