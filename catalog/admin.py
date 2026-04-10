e1efbb9cd9f6edc8f9bacd12b586010e2c0f2aef
Viktor Bukotin <viktorbukotin0@gmail.com>
2026-04-10 15:15:34 +0300

    23_to_revise

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,14 +1,39 @@
 from django.contrib import admin
-from catalog.models import Product, Category
+
+from catalog.models import Category, Contact, Product
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'category',)
-    list_filter = ('category',)
-    search_fields = ('name', 'description',)
+    list_display = (
+        "id",
+        "name",
+        "price",
+        "category",
+    )
+    list_filter = ("category",)
+    search_fields = (
+        "name",
+        "description",
+    )
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name',)
-    search_fields = ('name', 'description',)
+    list_display = (
+        "id",
+        "name",
+    )
+    search_fields = (
+        "name",
+        "description",
+    )
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "email",
+        "number",
+    )
--- catalog/views.py
+++ catalog/views.py
@@ -1,20 +1,26 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-from catalog.models import Product
+from catalog.models import Contact, Product
 
 
 def home(request: HttpRequest) -> HttpResponse:
-    """Функция, рендерирующая страницу 'home'"""
-    last_prod = Product.objects.order_by('-id')[:5]
+    """Функция, рендерирующая страницу 'home' и отображающая в консоли пять крайних добавленных товаров"""
+    last_prod = Product.objects.order_by("-id")[:5]
     for i in last_prod:
-        print(f'Товар: {i.name}, создан: {i.created_at}')
+        print(f"Товар: {i.name}, создан: {i.created_at}")
     return render(request, "catalog/home.html")
 
 
 def contacts(request: HttpRequest) -> HttpResponse:
     """Функция, рендерирующая страницу 'contacts'"""
-    return render(request, "catalog/contacts.html")
+    contact = Contact.objects.all()
+    context = {
+        "names": [i.name for i in contact][-3:],
+        "emails": [i.email for i in contact][-3:],
+        "numbers": [i.number for i in contact][-3:],
+    }
+    return render(request, "catalog/contacts.html", context=context)
 
 
 def contacts_post(request: HttpRequest) -> HttpResponse:
