6d6a0f9d2a5f4f309f2ff69b4bebe5d88fd239e3
Viktor Bukotin <viktorbukotin0@gmail.com>
2026-04-16 16:40:27 +0300

    24_to_revise

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
-from catalog.models import Contact, Product, Category
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from catalog.models import Category, Contact, Product
 
 
 def home(request: HttpRequest) -> HttpResponse:
@@ -10,10 +11,10 @@ def home(request: HttpRequest) -> HttpResponse:
     products = Product.objects.all()
 
     paginator = Paginator(products, 3)
-    page_number = request.GET.get('page')
+    page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
-    context = {'page_obj': page_obj}
+    context = {"page_obj": page_obj}
     for i in last_prod:
         print(f"Товар: {i.name}, создан: {i.created_at}")
     return render(request, "catalog/home.html", context)
@@ -43,19 +44,19 @@ def contacts_post(request: HttpRequest) -> HttpResponse:
 def product_detail(request: HttpRequest, pk: int) -> HttpResponse:
     """Функция, рендерирующая страницу информации детальной информации о товаре"""
     product = get_object_or_404(Product, id=pk)
-    context = {'product': product}
-    return render(request, 'catalog/product_detail.html', context)
+    context = {"product": product}
+    return render(request, "catalog/product_detail.html", context)
 
 
 def add_product(request: HttpRequest) -> HttpResponse:
     """Функция добавления товара в базу данных"""
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        description = request.POST.get('description')
-        image = request.POST.get('image')
-        price = request.POST.get('price')
-        input_category = request.POST.get('category')
+    if request.method == "POST":
+        name = request.POST.get("name")
+        description = request.POST.get("description")
+        image = request.POST.get("image")
+        price = request.POST.get("price")
+        input_category = request.POST.get("category")
         category = Category.objects.get_or_create(name=input_category)[0]
         Product.objects.create(name=name, description=description, image=image, price=price, category=category)
-        return render(request, 'catalog/success_add_product.html')
-    return render(request, 'catalog/add_product.html')
+        return render(request, "catalog/success_add_product.html")
+    return render(request, "catalog/add_product.html")
