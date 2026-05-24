777e7285d9dda99ccc508d210453579ba82b979f
Viktor Bukotin <viktorbukotin0@gmail.com>
2026-05-24 15:18:16 +0300

    task_1_2_is_ready

--- config/settings.py
+++ config/settings.py
@@ -109,3 +109,13 @@ LOGIN_REDIRECT_URL = "catalog:home"
 LOGOUT_REDIRECT_URL = "users:login"
 
 LOGIN_URL = "users:login"
+
+CACHE_ENABLE = True
+
+if CACHE_ENABLE:
+    CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+    }
+}
